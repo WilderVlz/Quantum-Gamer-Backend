551f6058c1fd29a348dd1a3248fcc57dc29147f0
Wilder <wilderruizrpg@gmail.com>
2023-10-12 17:24:00 -0500

    feat(PurchaseOrder) create entity & relationship

--- src/main/java/com/marcketplace/MarcketPlace/model/ShoppingCart.java
+++ src/main/java/com/marcketplace/MarcketPlace/model/ShoppingCart.java
@@ -13,22 +13,26 @@ import lombok.*;
 @Table(name = "shopping_cart")
 public class ShoppingCart implements Serializable {
     
-    private static final long serialVersionUID = 1L;
-    
+        private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     private int shoppingCartId;
-    
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User user;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", referencedColumnName = "product_id")
     private Product product;
     
-    @Column(name = "quantity", nullable = false)
-    private int quantity;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "purchase_order_id", referencedColumnName = "purchase_order_id")
+    private PurchaseOrder order;
     
 }
